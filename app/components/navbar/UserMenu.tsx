2583e130a776723c9a009580003ce23daba0ef46
EdLoth <eduardosram10@gmail.com>
2024-10-23 15:59:01 -0300

    Initi Prisma and models, Register and Login

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,9 +5,20 @@ import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { User } from '@prisma/client';
+import { signOut } from 'next-auth/react';
 
-const UserMenu = () => {
+interface UserMenuProps {
+  currentUser: User | null
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+  currentUser
+}) => {
   const registerModal = useRegisterModal()
+  const loginModal = useLoginModal()
+
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleOpen = useCallback(() => {
@@ -71,16 +82,47 @@ const UserMenu = () => {
         "
         >
           <div className='flex flex-col cursor-pointer'>
-            <>
-              <MenuItem
-              label='Login'
-              onClick={() => {}}
-              />
-               <MenuItem
-              label='Sign up'
-              onClick={registerModal.onOpen}
-              />
-            </>
+            {currentUser ? (
+              <>
+                <MenuItem
+                  label='My trips'
+                  onClick={() => {}}
+                />
+                <MenuItem
+                  label='My favorites'
+                  onClick={() => {}}
+                />
+                <MenuItem
+                  label='My reservations'
+                  onClick={() => {}}
+                />
+                <MenuItem
+                  label='My properties'
+                  onClick={() => {}}
+                />
+                <MenuItem
+                  label='Airbnb my home'
+                  onClick={() => {}}
+                />
+                <hr />
+                <MenuItem
+                  label='Logout'
+                  onClick={() => signOut()}
+                />
+              </>
+            ) : (
+              <>
+                <MenuItem
+                  label='Login'
+                  onClick={loginModal.onOpen}
+                />
+                <MenuItem
+                  label='Sign up'
+                  onClick={registerModal.onOpen}
+                />
+              </>
+            )}
+
           </div>
         </div>
       )}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,10 @@ import "./globals.css";
 import { Nunito } from 'next/font/google'
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({
   subsets: ['latin']
@@ -16,19 +18,23 @@ export const metadata: Metadata = {
 };
 
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const currentUser = await getCurrentUser();
+  
   return (
     <html lang="en">
       <body
         className={`${font.className}  antialiased`}
       >
         <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
