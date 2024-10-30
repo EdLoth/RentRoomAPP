aebdb96377bfadea17b126ef923395c7bc840683
Eduardo Ramos <eduardosram10@gmail.com>
2024-10-29 22:27:09 -0300

    Adjusts colors from pink to purple.

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -42,7 +42,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
         size={24}
         className={
           hasFavorited ?
-            'fill-rose-500'
+            'fill-purple-500'
             : 'fill-neutral-500/70'
         }
       />
